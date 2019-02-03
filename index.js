3097358c159caa8f55e08e5d1e57a8fe2f64d278
Toktam  Rezaei <toktamrezaei23@gmail.com>
2019-02-03 19:00:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet(feet) {
+  if (feet<=400) {
+    return "This one is on me!"
+  } else if (feet>2500) {
+    return 'No can do.'
+  } else if (feet>2000) {
+    return 'I will gladly take your thirty bucks.'
+  }
+}
+
+function ternaryCheckCity(city) {
+  return (city === "NYC") ? ("Ok, sounds good." ) : ("No go.")
+}
+
+ 
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+  case "generous" : {
+    return "Thank you so much." 
+    break
+  }
+}
+}
+
+
